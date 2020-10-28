9137f4790eafeaf237eef02aba45f03825f30405
JFronny <33260128+JFronny@users.noreply.github.com>
2020-10-28 09:47:04 +0100

    Slightly improve scripts using figlet

--- android/setup.sh
+++ android/setup.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 cd ~
-# Create user-facing scripts
+# Install dependencies
+pkg update -y
+pkg install proot-distro figlet wget
+
+figlet Setting up env
+proot-distro install ubuntu
+wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/master/android/setup2.sh?inline=false -O $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/setup2.sh
+chmod a+x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/setup2.sh
+proot-distro login ubuntu -- ./setup2.sh
+
+figlet Creating convenience scripts
 echo "#!/bin/sh" > smalipatcher-shell
 echo "proot-distro login ubuntu" >> smalipatcher-shell
 chmod a+x smalipatcher-shell
 echo "#!/bin/sh" > smalipatcher
 echo "proot-distro login ubuntu -- ./smalipatcher.sh \"\$@\"" >> smalipatcher
 chmod a+x smalipatcher
-# Install dependencies
-pkg update -y
-pkg install proot-distro
-# Set up proot
-proot-distro install ubuntu
-wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/master/android/setup2.sh?inline=false -O $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/setup2.sh
-chmod a+x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/setup2.sh
-proot-distro login ubuntu -- ./setup2.sh
+figlet Completed
