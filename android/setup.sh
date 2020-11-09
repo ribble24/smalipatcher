fb981a2021558a63f9f8ed2abc5524271fb7da8c
J. Fronny <6260391-JFronny@users.noreply.gitlab.com>
2020-11-09 06:05:55 +0000

    Specify ubuntu version

--- android/setup.sh
+++ android/setup.sh
@@ -5,16 +5,16 @@ pkg update -y
 pkg install proot-distro figlet wget
 
 figlet Setting up env
-proot-distro install ubuntu
+proot-distro install ubuntu-20.04
 wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/master/android/setup2.sh?inline=false -O $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/setup2.sh
-chmod a+x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/setup2.sh
-proot-distro login ubuntu -- ./setup2.sh
+chmod a+x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu-20.04/root/setup2.sh
+proot-distro login ubuntu-20.04 -- ./setup2.sh
 
 echo Creating convenience scripts
 echo "#!/bin/sh" > smalipatcher-shell
-echo "proot-distro login ubuntu" >> smalipatcher-shell
+echo "proot-distro login ubuntu-20.04" >> smalipatcher-shell
 chmod a+x smalipatcher-shell
 echo "#!/bin/sh" > smalipatcher
-echo "proot-distro login ubuntu -- ./smalipatcher.sh \"\$@\"" >> smalipatcher
+echo "proot-distro login ubuntu-20.04 -- ./smalipatcher.sh \"\$@\"" >> smalipatcher
 chmod a+x smalipatcher
 figlet Completed
