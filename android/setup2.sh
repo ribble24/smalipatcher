2879e3e09fdb9caf8f5cb69cc0a482c581568d30
JFronny <33260128+jfronny@users.noreply.github.com>
2020-11-15 14:28:32 +0100

    Attempt at locally downloading dotnet

--- android/setup2.sh
+++ android/setup2.sh
@@ -2,6 +2,11 @@
 unset LD_PRELOAD
 apt update -y
 apt install -y libssl-dev openjdk-14-jre-headless figlet zlib1g-dev
+figlet Insalling dotnet
+curl -SL -o dotnet.tar.gz https://download.visualstudio.microsoft.com/download/pr/4b114207-eaa2-40fe-8524-bd3c56b2fd9a/1d74fdea8701948c0150c39645455b2f/dotnet-runtime-5.0.0-linux-arm64.tar.gz
+mkdir -p /usr/share/dotnet
+tar -zxf dotnet.tar.gz -C /usr/share/dotnet
+ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
 figlet Fetching patcher
 wget -q https://gitlab.com/JFronny/smalipatcher/-/jobs/artifacts/master/download?job=android -O tmp.zip
 unzip tmp.zip
