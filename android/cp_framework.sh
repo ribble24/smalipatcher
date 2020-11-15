44d34e497c4325fad3d06c8d3952cf20ae8147a4
JFronny <33260128+jfronny@users.noreply.github.com>
2020-11-15 12:26:30 +0100

    Slightly more fancy cp_framework

--- android/cp_framework.sh
+++ android/cp_framework.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 rm -rf adb
 mkdir adb
-cp /system/build.prop adb/
-cp -R /system/framework adb/
-cp /system/system/build.prop adb/
-cp -R /system/system/framework adb/
+
+copyDir() {
+  if [[ -e "$1" && -r "$1" ]]
+  then
+    if [[ -d "$1" ]]
+    then
+      cp -R "$1" adb/
+    else
+      cp "$1" adb/
+    fi
+  fi
+}
+
+copyDir /system/build.prop
+copyDir /system/framework
+copyDir /system/system/build.prop
+copyDir /system/system/framework
