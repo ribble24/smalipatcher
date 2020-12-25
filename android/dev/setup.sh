77e5e593a9cbb113404038cd0a2f9c234f964b31
JFronny <33260128+JFronny@users.noreply.github.com>
2020-12-25 16:05:40 +0100

    add scripts to fetch dev builds

---
+++ android/dev/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cd ~ || exit
+# Install dependencies
+apt update
+apt upgrade -y
+apt install -y proot-distro figlet wget git make clang
+
+figlet Setting up env
+proot-distro install ubuntu-20.04
+wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/dev/android/dev/setup2.sh?inline=false -O $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu-20.04/root/setup2.sh
+chmod a+x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu-20.04/root/setup2.sh
+proot-distro login ubuntu-20.04 -- ./setup2.sh
+
+echo Creating convenience scripts
+echo "#!/bin/sh" > smalipatcher-shell
+echo "proot-distro login ubuntu-20.04" >> smalipatcher-shell
+chmod a+x smalipatcher-shell
+echo "#!/bin/sh" > smalipatcher
+echo "proot-distro login ubuntu-20.04 -- ./smalipatcher.sh \"\$@\"" >> smalipatcher
+chmod a+x smalipatcher
+figlet Completed
---
+++ android/dev/setup2.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+unset LD_PRELOAD
+apt update -y
+apt install -y libssl-dev openjdk-14-jre-headless figlet zlib1g-dev
+figlet Installing dotnet
+wget -q https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod a+x dotnet-install.sh
+./dotnet-install.sh -c 5.0 #TODO change if updated
+figlet Fetching patcher
+if [ "$(uname -m)" = "aarch64" ]
+then
+    wget -q https://gitlab.com/JFronny/smalipatcher/-/jobs/artifacts/dev/download?job=android -O tmp.zip
+elif [ "$(uname -m)" = "arm64" ]
+then
+    wget -q https://gitlab.com/JFronny/smalipatcher/-/jobs/artifacts/dev/download?job=android -O tmp.zip
+elif [ "$(uname -m)" = "armv7l" ]
+then
+    wget -q https://gitlab.com/JFronny/smalipatcher/-/jobs/artifacts/dev/download?job=android-arm -O tmp.zip
+elif [ "$(uname -m)" = "arm" ]
+then
+    wget -q https://gitlab.com/JFronny/smalipatcher/-/jobs/artifacts/dev/download?job=android-arm -O tmp.zip
+fi
+unzip tmp.zip
+rm tmp.zip
+figlet Building vdexExtractor
+git clone https://github.com/anestisb/vdexExtractor vdx
+cd vdx
+./make.sh
+cp bin/vdexExtractor ..
+cd ..
+rm -rf vdx
+echo Fetching scripts
+wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/dev/android/cp_framework.sh?inline=false -O cp_framework.sh
+chmod a+x cp_framework.sh
+wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/dev/android/smalipatcher.sh?inline=false -O smalipatcher.sh
+chmod a+x smalipatcher.sh
+echo Env setup complete
