3b7243410d6a6806c4060ab50a9cd58bb6e7b983
J. Fronny <6260391-JFronny@users.noreply.gitlab.com>
2021-08-17 17:09:07 +0000

    Update setup.sh

--- android/setup.sh
+++ android/setup.sh
@@ -6,16 +6,16 @@ apt upgrade -y
 apt install -y proot-distro figlet wget git make clang
 
 figlet Setting up env
-proot-distro install ubuntu-20.04
-wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/master/android/setup2.sh?inline=false -O $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu-20.04/root/setup2.sh
-chmod a+x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu-20.04/root/setup2.sh
-proot-distro login ubuntu-20.04 -- ./setup2.sh
+proot-distro install ubuntu
+wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/master/android/setup2.sh?inline=false -O $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/setup2.sh
+chmod a+x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/setup2.sh
+proot-distro login ubuntu -- ./setup2.sh
 
 echo Creating convenience scripts
 echo "#!/bin/sh" > smalipatcher-shell
-echo "proot-distro login ubuntu-20.04" >> smalipatcher-shell
+echo "proot-distro login ubuntu" >> smalipatcher-shell
 chmod a+x smalipatcher-shell
 echo "#!/bin/sh" > smalipatcher
-echo "proot-distro login ubuntu-20.04 -- ./smalipatcher.sh \"\$@\"" >> smalipatcher
+echo "proot-distro login ubuntu -- ./smalipatcher.sh \"\$@\"" >> smalipatcher
 chmod a+x smalipatcher
 figlet Completed
