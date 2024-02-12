f73b5d132b9b9740600c1c31b504af8ca7cee6cb
ribble24 <dcoquet@gmail.com>
2024-02-12 14:34:49 -0600

    Update setup.sh

--- android/setup.sh
+++ android/setup.sh
@@ -6,7 +6,7 @@ apt upgrade -y
 apt install -y proot-distro figlet wget git make clang
 
 figlet Setting up env
-proot-distro install ubuntu
+proot-distro install ubuntu-20.04
 wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/master/android/setup2.sh?inline=false -O $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/setup2.sh
 chmod a+x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/setup2.sh
 proot-distro login ubuntu -- ./setup2.sh
